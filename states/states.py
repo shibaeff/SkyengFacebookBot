5755f6a26f36ab900de22e988083940dde0943fb
Pavel Shibaev <shibaev.pp@phystech.edu>
2019-03-06 22:35:45 +0300

    minor fixes

--- states/states.py
+++ states/states.py
@@ -1,9 +1,11 @@
 # Onboarding states
 from state import State
+#TODO Finish the state
 initialState = State(outgoing="""1. Привет 👋. Рассказать вам немного о Skyeng?""" ,
                      user_variants=[],
                      next_states={"Нет": sadOkay})
 
+#TODO Finish the state
 sadOkay = State(outgoing="Передумали?",
                 user_variants=[],
                 next_states={"Передумал": initialState})
